546e42ad141f756353b9883f773cb356f0ea569a
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2023-04-15 22:21:11 +0700

    change some files

--- checkout.php
+++ checkout.php
@@ -7,7 +7,7 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Login Page</title>
+   <title>Checkout Page</title>
 
    <!-- custom css file link  -->
    <link rel="stylesheet" href="css/style.css">
@@ -19,7 +19,7 @@
 
    <div class="form-container">
 
-    <img src = 'images/BIDV.jpg'/>
+    <img src = 'images/BIDV.jpg' width = 500vh height = 500vh/>
 
    </div>
 
