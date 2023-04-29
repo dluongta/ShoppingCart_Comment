97aba688bf439dca49b5e33ba0b83e5d6583fed9
luen2003 <100186050+21020776TaDinhLuong@users.noreply.github.com>
2023-04-29 12:44:39 +0700

    change something

--- checkout.php
+++ checkout.php
@@ -144,30 +144,7 @@ if ($user_id != 1 && isset($_POST['order_btn'])) {
       </div>
 
    </header>
-
-   <div class="container">
-      <div class="user-profile">
-
-         <?php
-         $select_user = mysqli_query($conn, "SELECT * FROM `user_info` WHERE id = '$user_id'") or die('query failed');
-         if (mysqli_num_rows($select_user) > 0) {
-            $fetch_user = mysqli_fetch_assoc($select_user);
-         }
-         ;
-         ?>
-
-         <p> username : <span>
-               <?php echo $fetch_user['name']; ?>
-            </span> </p>
-         <div class="flex">
-            <a href="login.php" class="btn">login</a>
-            <a href="register.php" class="option-btn">register</a>
-            <a href="index.php?logout=<?php echo $user_id; ?>" class="delete-btn">logout</a>
-         </div>
-
-      </div>
-   </div>
-
+   
    <div class="container">
 
       <section class="checkout-form">
